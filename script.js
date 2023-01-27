5f653351ebb9270c98f677593d8397df0a579375
“Brenno <brennoeudes@gmail.com>
2023-01-27 12:11:29 -0300

    Images loop HTML_2

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const quizData = [
     c: "New Zealand",
     d: "England",
     correct: "c",
+    img: "https://upload.wikimedia.org/wikipedia/pt/e/e9/AucklandCityFC.png"
   },
   {
     question: "Where is the football team from?",
@@ -14,6 +15,7 @@ const quizData = [
     c: "Morocco",
     d: "Pakistan",
     correct: "b",
+    img: "https://w7.pngwing.com/pngs/148/452/png-transparent-al-hilal-fc-saudi-crown-prince-cup-football-esteghlal-f-c-al-duhail-sc-al-hilal-fc-emblem-trademark-logo.png"
   },
   {
     question: "Where is the football team from?",
@@ -22,6 +24,7 @@ const quizData = [
     c: "Chile",
     d: "Spain",
     correct: "d",
+    img: "https://upload.wikimedia.org/wikipedia/pt/9/98/Real_Madrid.png"
   },
   {
     question: "Where is the football team from?",
@@ -30,6 +33,7 @@ const quizData = [
     c: "Iran",
     d: "Saudi Arabia",
     correct: "b",
+    img: "https://seeklogo.com/images/A/Al_Ahly_Club-logo-BA70BE3AE8-seeklogo.com.png"
   },
   {
     question: "Where is the football team from?",
@@ -38,6 +42,7 @@ const quizData = [
     c: "Australia",
     d: "Netherlands",
     correct: "b",
+    img: "https://logodownload.org/wp-content/uploads/2021/01/seattle-sounders-fc-logo-1.png"
   },
   {
     question: "Where is the football team from?",
@@ -46,6 +51,7 @@ const quizData = [
     c: "Morocco",
     d: "Algeria",
     correct: "c",
+    img: "https://img2.gratispng.com/20180607/ysq/kisspng-wydad-ac-raja-casablanca-caf-champions-league-caf-5b197e6d7fdf08.8672573015283974215238.jpg"
   },
   {
     question: "Where is the football team from?",
@@ -54,6 +60,7 @@ const quizData = [
     c: "Paraguay",
     d: "Uruguay",
     correct: "a",
+    img: "https://logodownload.org/wp-content/uploads/2016/09/flamengo-logo-escudo-novo.png"
   },
 ];
 
@@ -65,6 +72,7 @@ const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const sumbmitBtn = document.getElementById("submit");
+const imagem = document.getElementById("image-shield");
 
 let currentQuiz = 0;
 let score = 0;
@@ -80,6 +88,7 @@ function loadQuiz() {
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
+  imagem.setAttribute("src", currentQuizData.img);
 }
 
 function deselectAnswers() {
